1fad59c974a625b6b76ac3be931299dfa9341d61
fabrivera <fabrivera.di@gmail.com>
2022-02-04 15:15:23 -0300

    nada

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export default {
     DB: {
-        URI: process.env.MONGODB_URI || 'mongodb+srv://new-db-mongo:6k58Z3g8pa9RwT3@cluster0.1v9de.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        URI: process.env.MONGODB_URI || 'mongodb+srv://api-movies:5hJ273WQGrZT9mn@cluster0.1v9de.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
         USER: process.env.USER,
         PASSWORD: process.env.PASSWORD
     },
