bfdc213f6ae3353a53b5642b0cb2eb9fc0c5206a
fabrivera <fabrivera.di@gmail.com>
2022-02-05 17:20:40 -0300

    cloudinary added

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -1,6 +1,6 @@
 import multer from 'multer'
-import { v4 as uuid } from 'uuid'
 import path from 'path'
+import cloudinary from 'cloudinary'
 
 export interface IMulter {
     single: string
@@ -9,7 +9,18 @@ export interface IMulter {
 const storage = multer.diskStorage({
     destination: 'uploads',
     filename: (req, file, callback) => {
-        callback(null, uuid() + path.extname(file.originalname))
+        cloudinary.v2.config({ 
+            cloud_name: 'des1wmoy4', 
+            api_key: '736815393194356', 
+            api_secret: 'XdkZenXHGfoEBc_jiUtsWYWZKbU' 
+        });
+        cloudinary.v2.uploader.upload(path.extname(file.originalname), result => {
+            if (result) {
+                const imgUrl = result.url
+                callback(null, imgUrl)
+            }
+            callback(null, 'null')
+        })
     }
 })
 
